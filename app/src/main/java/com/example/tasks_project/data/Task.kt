a07d1f25f7d99a153c848ffb30dcead0fd6533a1
mohamed <mohamed.jouini@esprit.tn>
2023-08-24 12:33:48 +0200

    layouts

---
+++ app/src/main/java/com/example/tasks_project/data/Task.kt
@@ -0,0 +1,19 @@
+package com.example.tasks_project.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+import androidx.versionedparcelable.VersionedParcelize
+
+
+
+import java.text.DateFormat
+
+@Entity(tableName="task_table")
+@VersionedParcelize
+ data class Task(val name: String, val imporant: Boolean = false,val completed: Boolean = false, val created: Long = System.currentTimeMillis(),@PrimaryKey(autoGenerate = true) val id :Int=0):Parcelable{
+    val createdDateFormatted: String
+        get() = DateFormat.getDateInstance().format(created)
+}
